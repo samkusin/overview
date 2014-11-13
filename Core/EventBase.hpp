abeced11b3f98af6fcf22237e7aeb153d77519ff
Samir Sinha <samir@cinekine.com>
2014-11-12 17:57:49 -0800

    Full definition of EventBase

--- Core/EventBase.hpp
+++ Core/EventBase.hpp
@@ -11,7 +11,32 @@
 #ifndef CK_Core_EventBase_hpp
 #define CK_Core_EventBase_hpp
 
+#include "cinek/types.hpp"
+
 namespace cinek {
+
+class EventBase
+{
+public:
+    EventBase(uint32_t timeMs);
+
+    virtual ~EventBase() {}
+    virtual const UUID& classID() const = 0;
+
+    uint32_t timestamp() const { return _ticks; }
+
+private:
+    uint32_t _ticks;
+};
+
+template<typename T> T* event_cast(const EventBase* evt) {
+    return (evt->classID() == T::kClassID) ? static_cast<T*>(evt) : nullptr;
+}
+
+inline EventBase::EventBase(uint32_t ticks) :
+    _ticks(ticks)
+{
+}
 
 } /* namespace cinek */
 
